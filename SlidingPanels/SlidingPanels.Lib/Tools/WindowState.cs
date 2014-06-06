ddf6985bc2bc4e5d9ec98246e5f7c9746b8c7e19
Molinet, Fabien <fabien.molinet@hager.fr>
2014-06-06 18:18:24 +0200

    missing files sorry

---
+++ SlidingPanels/SlidingPanels.Lib/Tools/WindowState.cs
@@ -0,0 +1,81 @@
+﻿// /// Copyright (C) 2013 Pat Laplante & Franc Caico
+// ///
+// ///	Permission is hereby granted, free of charge, to  any person obtaining a copy 
+// /// of this software and associated documentation files (the "Software"), to deal 
+// /// in the Software without  restriction, including without limitation the rights 
+// /// to use, copy,  modify,  merge, publish,  distribute,  sublicense, and/or sell 
+// /// copies of the  Software,  and  to  permit  persons  to   whom the Software is 
+// /// furnished to do so, subject to the following conditions:
+// ///
+// ///		The above  copyright notice  and this permission notice shall be included 
+// ///     in all copies or substantial portions of the Software.
+// ///
+// ///		THE  SOFTWARE  IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// ///     OR   IMPLIED,   INCLUDING  BUT   NOT  LIMITED   TO   THE   WARRANTIES  OF 
+// ///     MERCHANTABILITY,  FITNESS  FOR  A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// ///     IN NO EVENT SHALL  THE AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR ANY 
+// ///     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT 
+// ///     OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION  WITH THE SOFTWARE OR 
+// ///     THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// /// -----------------------------------------------------------------------------
+//
+using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using System.Linq;
+
+namespace SlidingPanels.Lib.Tools
+{
+	/// <summary>
+	/// Static class giving current screen figures regardless of the orientation.
+	/// </summary>
+	public static class WindowState
+	{
+		public static UIWindow Window {
+			get {
+				return UIApplication.SharedApplication.KeyWindow ?? UIApplication.SharedApplication.Windows.FirstOrDefault();
+			}
+		}
+
+		/// <summary>
+		/// Indicates if the device is in landscape mode.
+		/// </summary>
+		/// <value><c>true</c> if the device is in landscape mode; otherwise, <c>false</c>.</value>
+		public static bool IsLandscapeOrientation {
+			get {
+				return UIApplication.SharedApplication.StatusBarOrientation == UIInterfaceOrientation.LandscapeLeft || UIApplication.SharedApplication.StatusBarOrientation == UIInterfaceOrientation.LandscapeRight;
+			}
+		}
+
+		/// <summary>
+		/// Gives the real current window width which takes care of the current orientation.
+		/// </summary>
+		/// <value>Real current window width which takes care of the current orientation.</value>
+		public static float CurrentScreenWidth {
+			get {
+				if (IsLandscapeOrientation)
+					return Math.Max(Window.Bounds.Width, Window.Bounds.Height);
+				return Math.Min(Window.Bounds.Width, Window.Bounds.Height);
+			}
+		}
+
+		/// <summary>
+		/// Gives the real current window height which takes care of the current orientation.
+		/// </summary>
+		/// <value>Real current window height which takes care of the current orientation.</value>
+		public static float CurrentScreenHeight {
+			get {
+				if (IsLandscapeOrientation)
+					return Math.Min (Window.Bounds.Width, Window.Bounds.Height);
+				return Math.Max(Window.Bounds.Width, Window.Bounds.Height);
+			}
+		}
+
+		public static RectangleF CurrentScreenFrame {
+			get {
+				return new RectangleF (0, 0, CurrentScreenWidth, CurrentScreenHeight);
+			}
+		}
+	}
+}
+
